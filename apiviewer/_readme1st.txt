6b296b18a4dd6eedde74aac24ee50c791edf29d9
devicecontext <devicecontext@hotmail.com>
2018-01-21 16:34:35 -0500

    ApiViewer application

---
+++ apiviewer/_readme1st.txt
@@ -0,0 +1,1 @@
+ApiViewer for Visual FoxPro
---
+++ apiviewer/apiviewer.h
@@ -0,0 +1,39 @@
+#DEFINE _DEMO_VERSION 1
+
+#DEFINE APIVIEWER_PROTOCOL "apiviewer://"
+
+#DEFINE KEY_FUNCTION "fnc_"
+#DEFINE KEY_GROUP "grp_"
+#DEFINE KEY_LIBRARY "lib_"
+#DEFINE KEY_ABC "abc_"
+#DEFINE KEY_EXAMPLE "xmp_"
+#DEFINE KEY_CONSTANT "cns_"
+
+#DEFINE KEY_ROOT "root"
+
+#DEFINE GOOGLE_SEARCH "http://google.com/search?q="
+#DEFINE MSN_SEARCH "http://search.msn.com/results.aspx?q="
+#DEFINE MSDN_SEARCH "http://search.msdn.microsoft.com/search/results.aspx?qu="
+
+#DEFINE BLANK_PAGE_URL "about:blank"
+
+#DEFINE CRLF CHR(13)+CHR(10)
+#DEFINE BRTAG "<BR>"
+
+#DEFINE CONTACT_EMAIL "set in apiviewer.h"
+#DEFINE CONTACT_PERSON "set in apiviewer.h"
+#DEFINE CONTACT_PHONE "set in apiviewer.h"
+#DEFINE CONTACT_ADDRESS "set in apiviewer.h"
+#DEFINE COPYRIGHT_COMPANY "VFP community"
+#DEFINE COPYRIGHT_TEXT "Free for personal, non-commercial use" + COPYRIGHT_COMPANY
+
+#DEFINE PRODUCT_NAME "ApiViewer for Visual FoxPro"
+
+#DEFINE APIVIEWER_CSS "apiviewer.css"
+
+#DEFINE GENERATEDFILE_PREFIX "apiviewer_"
+#DEFINE GENERATEDFILE_EXT ".htm"
+
+#DEFINE UPDATE_REQUEST_URL "no longer applicable"
+
+#DEFINE APIVIEWER_BUFFER_BACKUP "apiviewer_buffer.txt"
